711fc88131c3a6817943ceb882ec5ecea74d7bec
ThePortalBox <75244896+ThePortalBox@users.noreply.github.com>
2025-04-27 15:26:09 -0600

    Update Config.cs

--- SpotLastSurvivor/Config.cs
+++ SpotLastSurvivor/Config.cs
@@ -14,7 +14,7 @@ namespace SpotLastSurvivor
         public bool CassieScanAnnounce { get; set; } = true;
 
         [Description("Cassie scan message")]
-        public string CassieAnnounceMessage { get; set; } = "SCANNING FOR LAST HUMAN";
+        public string CassieAnnounceMessage { get; set; } = "EVALUATING BREACH SEVERITY: SCANNING FOR SURVIVORS";
 
         [Description("Time cassie take before telling where the last survivor is (seconds) after the scan")]
         public float ScanningTime { get; set; } = 30f;
